55b664407600f88c5607fe16211422a82f4f2872
Jamie Poole <jep349@cornell.edu>
2020-05-19 05:24:16 -0400

    Update audio-test-new.py

--- audio-test-new.py
+++ audio-test-new.py
@@ -1,11 +1,10 @@
 import os
 import time
-import pygame
-from pygame.locals import *   # for event MOUSE variables
 
-pygame.init()
 
 #record audio
-cmd1='arecord -D hw:1,0 -d 5 -f cd /home/pi/Final/osaudiotest.wav -r 48000 -c 1 &'
+#cmd1='arecord -D hw:1,0 -d 5 -f cd /home/pi/Final/osaudiotest.wav -r 48000 -c 1 &'
+
+cmd1 = 'arecord -D hw:1,0 -d 5 -f S24_3LE /home/pi/Final/osaudiotest.wav -c2 -r48000 &'
 os.system(cmd1)
 print("recording...")
