9596afecd9f32b344055125d49c1efa7efc9f1c9
Jamie Poole <jep349@cornell.edu>
2020-05-20 11:57:51 -0400

    working concatenation
    first code segment works if you just want to combine everything together, test to work with trellis

--- test-concatenate-wavs.py
+++ test-concatenate-wavs.py
@@ -1,14 +1,14 @@
-import RPi.GPIO as GPIO
 import os
+import time
+from pydub import AudioSegment
+from pydub.playback import play
 import pygame
 import time
 import busio
 from board import SCL, SDA
 from adafruit_trellis import Trellis
 from pygame.locals import *   # for event MOUSE variables
-import setting
-from pydub import AudioSegment
-from pydub.playback import play
+#import setting
 
 # Create the I2C interface
 i2c = busio.I2C(SCL, SDA)
@@ -16,44 +16,60 @@ i2c = busio.I2C(SCL, SDA)
 # Create a Trellis object
 trellis = Trellis(i2c)  # 0x70 when no I2C address is supplied
 
+
+#working concatenation code
+#this worked on my mac at least lol i just added in the path stuff
 # instrument files
-wavefiles = ['01.wav','02.wav','03.wav','04.wav','05.wav','06.wav','07.wav','08.wav',
-  '09.wav','10.wav','11.wav','12.wav','13.wav','14.wav','15.wav','16.wav']
+wavefiles = ['01.wav','02.wav', '03.wav', '04.wav','05.wav', '06.wav', '07.wav','08.wav', 
+			'09.wav', '10.wav','11.wav', '12.wav', '13.wav','14.wav', '15.wav', '16.wav']
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+path = "/home/pi/final/violin/" #I tested using violin sounds
 
-def GPIO27_callback(channel):
-    exit(0)
-    print(27)
+#testing concatenation once
+'''
+combined = AudioSegment.from_wav(path + wavefiles[0])
+
+for wav in wavefiles[1:]:
+    combined += AudioSegment.from_wav(path + wav)
+
+play(combined)'''
+
+print("Starting button sensory loop...")
+pressed_buttons = set()
+
+#pygame.mixer.pre_init(44100,16,2,4096)
+pygame.init()
+pygame.mixer.init()
+
+#def concatenate_wav():
+	#wavs = [AudioSegment.from_wav(wav['path']) for wav in wavset]
+
+while True:
+    # Make sure to take a break during each trellis.read_buttons
+    # cycle.
+    time.sleep(0.1)
     
-def GPIO19_callback(channel):
-    print("callback 19")
-    #cmd1 = 'arecord -D hw:1,0 -d 5 -f S24_3LE /home/pi/Final/loop1.wav -c2 -r48000 &'
-    #os.system(cmd1)
-    concatenate_wav("/home/pi/Final/loop1.wav", wavefiles)
-    loop1 = AudioSegment.from_wav("output.wav")
-    play(mixed)
+    just_pressed, released = trellis.read_buttons()
+    combined = AudioSegment.from_wav(path + wavefiles[0])
+    for b in just_pressed:
+        combined += AudioSegment.from_wav(path + wavefiles[b]) #add pressed file to concatenation
+        #pygame.mixer.music.load(name)
+        #pygame.mixer.music.play(0)
+        print("pressed:", b)
+        trellis.led[b] = True
+    pressed_buttons.update(just_pressed)
+    for b in released:
+        print("released:", b)
+        trellis.led[b] = False
+    pressed_buttons.difference_update(released)
+    for b in pressed_buttons:
+        print("still pressed:", b)
+        trellis.led[b] = True
+
+    play(combined)
 
 
-def GPIO26_callback(channel):
-    print("callback 26")
-    cmd2 = 'arecord -D hw:1,0 -d 5 -f S24_3LE /home/pi/Final/loop2.wav -c2 -r48000 &'
-    os.system(cmd2)
 
-GPIO.add_event_detect(27,GPIO.FALLING, callback=GPIO27_callback)
-GPIO.add_event_detect(19,GPIO.FALLING, callback=GPIO19_callback)
-GPIO.add_event_detect(26,GPIO.FALLING, callback=GPIO26_callback)
-
-
-def concatenate_wav(orders, wavs):                                                  
-    combined_wav = AudioSegment.empty()
-	for order, wav in zip(orders, wavs):                                                            
-    	order = AudioSegment.from_wav(wav)                                                          
-    	combined_wav += order      
-        combined_wav.export(os.path.join(os.path.dirname(__file__), "output.wav"), format="wav")
 
 
 
