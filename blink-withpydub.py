fa31d52a0c72a8f040687cb186b7ff3b71784598
Vsj986 <51970741+Vsj986@users.noreply.github.com>
2020-05-20 08:07:46 -0400

    working 2-mode

--- blink-withpydub.py
+++ blink-withpydub.py
@@ -24,8 +24,43 @@ GREEN = 0, 128, 0
 RED = 255, 0, 0
 CYAN = 109, 237, 226 #for background of TFT screen
 screen = pygame.display.set_mode((320, 240))
-my_font= pygame.font.Font(None, 30)
+my_font= pygame.font.Font(None, 25)
 other_font= pygame.font.Font(None, 20)
+
+#initialize the screen
+instrument_buttons = ['piano', 'violin', 'flute', 'drum']
+instrument_index = 0    #piano default
+button_states = ['stopped', 'recording', 'playback']
+
+global state1
+global state2
+global mode
+mode = 1
+state1 = 0
+state2 = 0
+
+my_buttons= {(60,120):instrument_buttons[instrument_index], (270,200):'quit',
+             (200,60):'wait for ready', (200,110):'click here to switch mode',
+             (200,140): "cha1: not recording", (200,160): "cha2: not recording"}
+
+screen.fill(CYAN)               # Erase the Work space     
+for text_pos, my_text in my_buttons.items():
+    if text_pos == (200,60):
+        font = my_font
+        color = RED
+    elif text_pos == (200,110):
+        font = other_font
+        color = RED
+    else:
+        font = other_font
+        color = BLACK
+    text_surface = font.render(my_text, True, color)    
+    rect = text_surface.get_rect(center=text_pos)
+    screen.blit(text_surface, rect)
+    
+pygame.draw.polygon(screen, BLACK, ((60,50),(50,60),(70,60))) #up arrow
+pygame.draw.polygon(screen, BLACK, ((60,190),(50,180),(70,180))) #down arrow
+pygame.display.flip()
 
 
 # Create the I2C interface
@@ -56,29 +91,25 @@ GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#initialize the screen
-instrument_buttons = ['piano', 'violin', 'flute', 'drum']
-instrument_index = 0    #piano default
-button_states = ['stopped', 'recording', 'playback']
-
-global state1
-global state2
-state1 = 0
-state2 = 0
-
-my_buttons= {(80,120):instrument_buttons[instrument_index], (270,200):'quit', 
-                (200,100): button_states[state1], (200,120): button_states[state2]}
-
-screen.fill(CYAN)               # Erase the Work space     
-for text_pos, my_text in my_buttons.items():    
-    text_surface = other_font.render(my_text, True, WHITE)    
-    rect = text_surface.get_rect(center=text_pos)
-    screen.blit(text_surface, rect)
+def update_screen():
+    screen.fill(CYAN)
+    pygame.draw.polygon(screen, BLACK, ((60,50),(50,60),(70,60))) #up arrow
+    pygame.draw.polygon(screen, BLACK, ((60,190),(50,180),(70,180))) #down arrow
+    for text_pos, my_text in my_buttons.items():
+        if text_pos == (200,60):
+            font = my_font
+            color = RED
+        elif text_pos == (200,110):
+            font = other_font
+            color = RED
+        else:
+            font = other_font
+            color = BLACK
+        text_surface = font.render(my_text, True, color)  
+        rect = text_surface.get_rect(center=text_pos)
+        screen.blit(text_surface, rect)
+    pygame.display.flip()
     
-pygame.draw.polygon(screen, WHITE, ((80,50),(70,60),(90,60))) #up arrow
-pygame.draw.polygon(screen, WHITE, ((80,190),(70,180),(90,180))) #down arrow
-pygame.display.flip()
-
 # blink on key pad
 # Turn on every LED, one at a time
 print("Turning on each LED, one at a time...")
@@ -99,6 +130,10 @@ pressed_buttons = set()
 pygame.init()
 pygame.mixer.init()
 
+# looper is reay
+my_buttons[(200,60)] = 'Current mode: Keypad'
+update_screen()
+    
 # set up GPIO callback functions
 def GPIO27_callback(channel):
     exit(0)
@@ -106,68 +141,67 @@ def GPIO27_callback(channel):
     
 def GPIO19_callback(channel):
     print("callback 19")
+    global mode
+
+    my_buttons[(200,140)] = "cha1: recording"
+    update_screen()
     cmd1 = 'arecord -D hw:1,0 -d 5 -f S24_3LE /home/pi/Final/loop1.wav -c2 -r48000 &'
     os.system(cmd1)
-    ################## see if buttons get updated
-    my_buttons[(200,100)] = "testing"
-    print("19_my buttons:",my_buttons)
+    time.sleep(5)
+    my_buttons[(200,140)] = "cha1: not recording"
+    update_screen()
+    
+
 
 def GPIO26_callback(channel):
     print("callback 26")
+    global mode
+    mode = 1
+    my_buttons[(200,160)] = "cha2: recording"
+    update_screen()
     cmd2 = 'arecord -D hw:1,0 -d 5 -f S24_3LE /home/pi/Final/loop2.wav -c2 -r48000 &'
     os.system(cmd2)
-    ################## see if buttons get updated
-    my_buttons[(200,120)] = "testing"
-    print("19_my buttons:",my_buttons)
+    time.sleep(5)
+    mode = 2
+    my_buttons[(200,160)] = "cha2: not recording"
+    update_screen()
+    
+ 
 
 GPIO.add_event_detect(27,GPIO.FALLING, callback=GPIO27_callback)
 GPIO.add_event_detect(19,GPIO.FALLING, callback=GPIO19_callback)
 GPIO.add_event_detect(26,GPIO.FALLING, callback=GPIO26_callback)
-
-
+    
 while True:
     # Make sure to take a break during each trellis.read_buttons
     # cycle.
     time.sleep(0.1)
     
-    just_pressed, released = trellis.read_buttons()
-    for b in just_pressed:
-        name = '/home/pi/Final' + paths[3] + wavefiles[b]
-        pygame.mixer.Channel(0).play(pygame.mixer.Sound(name))
-        #pygame.mixer.music.load(name)
-        #pygame.mixer.music.play(0)
-        print("pressed:", b)
-        trellis.led[b] = True
-    pressed_buttons.update(just_pressed)
-    for b in released:
-        print("released:", b)
-        trellis.led[b] = False
-    pressed_buttons.difference_update(released)
-    for b in pressed_buttons:
-        print("still pressed:", b)
-        trellis.led[b] = True
-    
     #change GUI
     for event in pygame.event.get():        
         if(event.type is MOUSEBUTTONDOWN):            
             pos = pygame.mouse.get_pos()
-            screen.fill(CYAN)
-            for text_pos, my_text in my_buttons.items():    
-                text_surface = other_font.render(my_text, True, WHITE)    
-                rect = text_surface.get_rect(center=text_pos)
-                screen.blit(text_surface, rect)
-            pygame.draw.polygon(screen, WHITE, ((80,50),(70,60),(90,60))) #up arrow
-            pygame.draw.polygon(screen, WHITE, ((80,190),(70,180),(90,180))) #down arrow
+            update_screen()
+#             screen.fill(CYAN)
+#             for text_pos, my_text in my_buttons.items():    
+#                 text_surface = other_font.render(my_text, True, WHITE)    
+#                 rect = text_surface.get_rect(center=text_pos)
+#                 screen.blit(text_surface, rect)
+#             pygame.draw.polygon(screen, WHITE, ((80,50),(70,60),(90,60))) #up arrow
+#             pygame.draw.polygon(screen, WHITE, ((80,190),(70,180),(90,180))) #down arrow
         elif(event.type is MOUSEBUTTONUP):            
             pos = pygame.mouse.get_pos() 
             x,y = pos
-            if y > 190 and y < 210 and x > 250 and x < 290: #if quit button           
+            if y > 190 and y < 210 and x > 250 and x < 290: #if quit button
+                mode = 1
                 exit(0)
             elif y > 40 and y < 70 and x > 60 and x < 100: #if up arrow
                 if(instrument_index == 3): #out of bounds
                     instrument_index = 0
                 else:
                     instrument_index = instrument_index + 1
+                my_buttons[(80,120)] = instrument_buttons[instrument_index]
+                update_screen()
 
                 print('up arrow')
                 print(instrument_index)
@@ -177,52 +211,47 @@ while True:
                     instrument_index = 3
                 else:
                     instrument_index = instrument_index - 1
+                my_buttons[(80,120)] = instrument_buttons[instrument_index]
+                update_screen()
                 print('down arrow')
                 print(instrument_index)
                 print(instrument_buttons[instrument_index])
+            elif y > 100 and y < 120 and x > 100 and x < 300: #if switch mode button
+                if mode == 1:
+                    mode = 2 # looper mode
+                    my_buttons[(200,60)] = "Current mode: Looper"
+                elif mode == 2:
+                    mode = 1 # keypad mode
+                    my_buttons[(200,60)] = "Current mode: Keypad"
+                update_screen()
+    if mode == 1:
+        just_pressed, released = trellis.read_buttons()
+        for b in just_pressed:
+            name = '/home/pi/Final' + paths[3] + wavefiles[b]
+            pygame.mixer.Channel(0).play(pygame.mixer.Sound(name))
+            #pygame.mixer.music.load(name)
+            #pygame.mixer.music.play(0)
+            print("pressed:", b)
+            trellis.led[b] = True
+        pressed_buttons.update(just_pressed)
+        for b in released:
+            print("released:", b)
+            trellis.led[b] = False
+        pressed_buttons.difference_update(released)
+        for b in pressed_buttons:
+            print("still pressed:", b)
+            trellis.led[b] = True
+    if mode == 2:
+        # mixing channel 1 and 2 together
+        loop1 = AudioSegment.from_wav("/home/pi/Final/loop1.wav")
+        loop2 = AudioSegment.from_wav("/home/pi/Final/loop2.wav")
+        length = len(loop1)
+        mixed = loop2[:length].overlay(loop1)
+        play(mixed)
             
-    # update record buttons
-    
-    # updating the screen when a record button is pressed
-    if GPIO.input(19) == GPIO.LOW or GPIO.input(26) == GPIO.LOW:
-        print("line 188")
-        print("line 189, my buttons: ", my_buttons)
-        screen.fill(CYAN)
-        pygame.draw.polygon(screen, WHITE, ((80,50),(70,60),(90,60))) #up arrow
-        pygame.draw.polygon(screen, WHITE, ((80,190),(70,180),(90,180))) #down arrow
-                    
-        #my_buttons= {(80,120):instrument_buttons[instrument_index], (270,200):'quit',
-        #            (200,100): button_states[state1], (200,120): button_states[state2]}
 
-        for text_pos, my_text in my_buttons.items():    
-            text_surface = other_font.render(my_text, True, WHITE)    
-            rect = text_surface.get_rect(center=text_pos)
-            screen.blit(text_surface, rect)
-        pygame.display.flip()
-             
-    else:
-        screen.fill(CYAN)
-        pygame.draw.polygon(screen, WHITE, ((80,50),(70,60),(90,60))) #up arrow
-        pygame.draw.polygon(screen, WHITE, ((80,190),(70,180),(90,180))) #down arrow
-                    
-        my_buttons= {(80,120):instrument_buttons[instrument_index], (270,200):'quit',
-                        (200,100): button_states[state1], (200,120): button_states[state2]}
+            
 
-        for text_pos, my_text in my_buttons.items():    
-            text_surface = other_font.render(my_text, True, WHITE)    
-            rect = text_surface.get_rect(center=text_pos)
-            screen.blit(text_surface, rect)
-        pygame.display.flip()
+
         
-        
-    # mixing channel 1 and 2 together
-    loop1 = AudioSegment.from_wav("/home/pi/Final/loop1.wav")
 
-    loop2 = AudioSegment.from_wav("/home/pi/Final/loop2.wav")
-
-    length = len(loop1)
-
-    mixed = loop2[:length].overlay(loop1)
-    
-    print("playing mixed")
-    play(mixed)
